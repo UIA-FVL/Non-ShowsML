164f6f5b067e5c631090d4ffb9aef3f975d25aaf
Unidad de Inteligencia Artificial- Fundación Valle del Lili <165051814+UIA-FVL@users.noreply.github.com>
2025-01-14 08:36:05 -0500

    Update Stadistics.R

--- NonShowsCodes/DataAnalysis/Stadistics.R
+++ NonShowsCodes/DataAnalysis/Stadistics.R
@@ -1,7 +1,7 @@
-## Limpieza del ambiente
-rm(list=ls())
+## Cleaning the environment
+rm(list = ls())
 
-## Carga de librerias
+## Loading libraries
 library(readxl)
 library(dplyr)
 library(ggplot2)
@@ -11,22 +11,21 @@ library(labelled)
 library(dplyr)
 library(Hmisc)
 
-## Carga de datos
+## Loading data
 data <- readxl::read_xlsx("data/df.xlsx")
 
-## Resumen de los datos 
+## Summary of the data
 summary(data)
 names(data)
 str(data)
 
-## Renombrar columnas en formato unificado
+## Renaming columns to a unified format
 data <- data %>%
   rename_with(~tolower(gsub(" ", "_", .)) %>%
                 gsub("\\.", "", .) %>%
                 gsub("-", "", .), .cols = everything())
 
-
-# Asignando etiquetas y convirtiendo las variables categóricas en factores con sus niveles y etiquetas correspondientes
+# Assigning labels and converting categorical variables into factors with their corresponding levels and labels
 data <- data %>%
   mutate(
     tipo_de_cita = factor(tipo_de_cita,
@@ -48,14 +47,14 @@ data <- data %>%
                              "12:00 to 14:00", "14:00 to 16:00", "16:00 to 18:00")),
   )
 
-# Asignando etiquetas a cada variable con Hmisc::label()
+# Assigning labels to each variable with Hmisc::label()
 data <- within(data, {
   label(tipo_de_cita) <- "Type of appointment"
   label(edad) <- "Age"
   label(sexo) <- "Sex"
   label(aseguradora_tipo) <- "Type of insurance coverage"
   label(numero_dx) <- "Number of diagnoses"
-  label(hosp_reciente) <- "Number of recent hospitalization"
+  label(hosp_reciente) <- "Number of recent hospitalizations"
   label(numero_de_mx) <- "Number of medications"
   label(hora) <- "Hour of appointment"
   label(día_cita) <- "Day of the appointment"
@@ -65,21 +64,17 @@ data <- within(data, {
   label(inasistencias_previas) <- "Number of previous absences"
 })
 
-
 table(data$hosp_reciente)
 
-
-
-## Resumen de los datos
+## Summary of the data
 names(data)
 str(data)
 
-
-# Crear la Tabla 1
+# Create Table 1
 table1 <- data %>%
   tbl_summary(
     by = tipo_de_cita,
-    # Establecer todas las variables como continuas
+    # Set all variables as continuous
     type = list(
       edad ~ "continuous2",
       numero_dx ~ "continuous2",
@@ -89,10 +84,10 @@ table1 <- data %>%
       asistencias_previas ~ "continuous2",
       inasistencias_previas ~ "continuous2"
     ),
-    # Definir las estadísticas para las variables continuas
+    # Define statistics for continuous variables
     statistic = list(
       all_continuous() ~ c("{mean} ({sd})", "{median} ({p25}, {p75})"),
-      # Aquí se asume que las demás variables no listadas son categóricas
+      # Here it's assumed that other non-listed variables are categorical
       all_categorical() ~ "{n} ({p}%)"
     ),
     digits = everything() ~ 2
@@ -101,7 +96,7 @@ table1 <- data %>%
   add_overall() %>%
   add_q()
 
-# Ver la Tabla 1
+# View Table 1
 table1
 
 
